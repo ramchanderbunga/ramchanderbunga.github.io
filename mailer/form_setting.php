1d53122760b85fd761fe81f768e67145fc8c2df5
ramchanderbunga <ramchander.bunga@gmail.com>
2024-01-11 19:54:36 +0530

    updated home page

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,7 +1,7 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'Ober - CV Resume Template'"; //letter subject
-	$to = 'youremail@domain.com'; // Enter Your E-mail
+	$subj = "New message from the site 'Cvio - Resume/CV Template'"; //letter subject
+	$to = 'beshleyua@gmail.com'; // Enter Your E-mail
 	$from = 'admin@you-site-name.com'; // Admin e-mail
 	$fromName = 'Your Company Name'; // Your company name
 	$charset = 'UTF-8';
